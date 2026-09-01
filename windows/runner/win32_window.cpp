4d09ab8610da7dcabe70cac61b0f466067aa3037
vicentegmj <vicente.gmj@gmail.com>
2026-09-01 16:40:39 -0300

    Faz a aplicação sempre abrir maximizada
    Troca SW_SHOWNORMAL por SW_SHOWMAXIMIZED na exibição inicial da janela
    do runner do Windows.

--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -150,7 +150,8 @@ bool Win32Window::Create(const std::wstring& title,
 }
 
 bool Win32Window::Show() {
-  return ShowWindow(window_handle_, SW_SHOWNORMAL);
+  // A aplicação deve sempre abrir maximizada.
+  return ShowWindow(window_handle_, SW_SHOWMAXIMIZED);
 }
 
 // static
